6597e58a3a97301eb753c6a26db0b2c1e6f97006
sachin41 <baralisachin472@gmail.com>
2023-07-28 23:38:14 +0545

    expense tracker

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import ExpenseItem from './ExpenseItem'
+import './Expenses.css'
+import Card from '../UI/Card'
+
+const Expenses = (props) => {
+    const expenses = props.expense
+    return (
+        <Card className='expenses'>
+            <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} />
+            <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} />
+            <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date} />
+            <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date} />
+        </Card>
+    )
+}
+
+export default Expenses
