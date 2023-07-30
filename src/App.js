ed95ffe6570064bd133b06bd383a2103d251f276
sachin41 <baralisachin472@gmail.com>
2023-07-30 21:27:44 +0545

    state and newExpense UI

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // import ExpenseItem from "./components/ExpenseItem";
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
   const expenses = [
@@ -31,7 +32,7 @@ function App() {
   return (
 
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense/>
       <Expenses expense={expenses}/>
     </div>
   );
