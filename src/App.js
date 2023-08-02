a08bbe52d46704763049bd0813e2d0635ea8540f
sacheen <baralisachin@gmail.com>
2023-08-02 21:30:19 +0545

    rendering list and conditional statement

--- src/App.js
+++ src/App.js
@@ -1,48 +1,54 @@
+import { useState } from "react";
 // import ExpenseItem from "./components/ExpenseItem";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
+const DUMMY_DATA = [
+  {
+    id: 'e1',
+    title: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date(2023, 7, 14),
+  },
+  {
+    id: 'e2',
+    title: 'New TV',
+    amount: 799.49,
+    date: new Date(2022, 2, 12),
+  },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2022, 2, 28),
+  },
+  {
+    id: 'e4',
+    title: 'New Desk (Wooden)',
+    amount: 450,
+    date: new Date(2024, 5, 12),
+  },
+];
+
+
 function App() {
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2021, 7, 14),
-    },
-    { 
-      id: 'e2', 
-      title: 'New TV', 
-      amount: 799.49,
-      date: new Date(2022, 2, 12), 
-    },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2022, 2, 28),
-    },
-    {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
-      amount: 450,
-      date: new Date(2024, 5, 12),
-    },
-  ];
+
+  const [expenses,setExpenses] = useState(DUMMY_DATA);
 
   //function to add newExpense
 
-  const addExpenseHandler = expense =>{
-    console.log('In App.js');
-    console.log(expense);
+  const addExpenseHandler = expense => {
+    setExpenses(prevExpenses => {
+      return [expense,...prevExpenses];
+    });
   }
 
 
   return (
 
     <div>
-      <NewExpense onAddExpenseData = {addExpenseHandler}/>
-      <Expenses expense={expenses}/>
+      <NewExpense onAddExpenseData={addExpenseHandler} />
+      <Expenses expense={expenses} />
     </div>
   );
 }
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,24 +1,40 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import ExpenseItem from './ExpenseItem'
 import './Expenses.css'
 import Card from '../UI/Card'
 import ExpensesFilter from './ExpenseFilter'
 
 const Expenses = (props) => {
-    const [filteredYear , setFilteredYear] = useState('2021');
+
+    const expenses = props.expense;
+    const [filteredYear, setFilteredYear] = useState('2021');
 
     const filterChangeHandler = selectedYear => {
         setFilteredYear(selectedYear);
     }
-    const expenses = props.expense
+
+
+    //logic to filter the data according to selectedYear
+
+    const filteredExpenses = expenses.filter(expense => {
+        return expense.date.getFullYear().toString() === filteredYear;
+    });
+
     return (
         <div>
             <Card className='expenses'>
-                <ExpensesFilter selected = {filteredYear} onFilterChange = {filterChangeHandler}/>
-                <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} />
-                <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} />
+                <ExpensesFilter selected={filteredYear} onFilterChange={filterChangeHandler} />
+                {filteredExpenses.length === 0 ? <h1 className='expenses__empty'>No items found</h1> : filteredExpenses.map((item) => {
+                    return <ExpenseItem
+                        key={item.id}
+                        title={item.title}
+                        amount={item.amount}
+                        date={item.date} />
+                })};
+
+                {/* <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} />
                 <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date} />
-                <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date} />
+                <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date} /> */}
             </Card>
         </div>
     )
